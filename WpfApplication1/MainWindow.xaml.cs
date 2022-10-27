bc48bd3ec427ba2a3e287caafd4c46898f59b877
SakkeSar <sakkesarkkinen2@gmail.com>
2022-10-27 09:54:42 +0300

    moi3

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApplication1
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show(Laatikko1.Text + Laatikko2.Text);
         }
     }
 }
